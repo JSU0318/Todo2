a4aef96ea0a88fc34917b2e579606bd8ec9dccb7
JSU0318 <dnr0000@gmail.com>
2022-12-01 11:27:42 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+function User(props) {
+  return <div className="square-style">{props.user.title}</div>;
+}
 
 function App() {
+  const [users, setUsers] = useState([{ id: 1, title: "react를 배워봅시다" }]);
+
+  const [title, setTitle] = useState("");
+
+  const addUserHandler = () => {
+    const newUser = {
+      id: users.length + 1,
+      title: title,
+    };
+
+    setUsers([...users, newUser]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-content">
+        <div className="title">
+          <input
+            className="input-box"
+            value={title}
+            placeholder=""
+            // 인풋 이벤트로 들어온 입력 값을 name의 값으로 업데이트
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button onClick={addUserHandler} padding>
+          추가하기
+        </button>
+      </div>
+      <span className="h4">
+        <h4>Todo List</h4>
+      </span>
+      <ul className="app-style">
+        {users.map((user) => {
+          return <User user={user} key={user.id}></User>;
+        })}
+      </ul>
     </div>
   );
 }
